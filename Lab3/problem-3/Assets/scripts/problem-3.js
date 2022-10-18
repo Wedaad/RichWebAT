aa35cff5b36e52338107cbb24d49970f6cc329fa
Wedaad <wedaadh2@gmail.com>
2022-10-18 23:27:17 +0100

    add: displayUserRepos() now implemented

--- Lab3/problem-3/Assets/scripts/problem-3.js
+++ Lab3/problem-3/Assets/scripts/problem-3.js
@@ -1,5 +1,6 @@
 console.log("Javascript loaded");
 
+//FUNCTION: TO SEARCH GITHUB API FOR USERNAME THAT IS ENTERED
 function searchUser() {
     
     const search_input = document.getElementById("search-query");
@@ -7,6 +8,11 @@ function searchUser() {
     console.log("INSIDE SEARCH USER");
     let username = document.getElementById('search-query').value;
     console.log("USERNAME: " + username);
+
+    // if(!username) {
+
+        // TO-DO: ADD VALIDATION
+    // }
 
     search_input.value = ""; //resetting input field to be empty once search
     
@@ -16,23 +22,22 @@ function searchUser() {
 
         console.log(response.json());
 
+        //if the request has a status code of 200 - 299
         if(response.ok) {
 
-            console.log("USER FOUND");
             displayUserProfile(username);
             displayUserRepos(username);
 
-        } else {
-
-            console.log("USER NOT FOUND");
         }
 
+    }).catch(function (err) {
 
-    })
+        console.log("ERROR: USER NOT FOUND." + err);
+    });
 
 }
 
-// populate web application
+// FUNCTION: TO DISPLAY USER INFORMATION
 function displayUserProfile(username) {
 
     let profile_picture = document.getElementById('profile-picture');
@@ -47,45 +52,59 @@ function displayUserProfile(username) {
     .then(user_data => {
 
         profile_picture.src = user_data.avatar_url;
-        name.innerHTML += user_data.name;
-        git_username.innerHTML += user_data.login;
-        location.innerHTML += user_data.location;
-        num_gists.innerHTML += user_data.public_gists;
+        name.insertAdjacentText('beforeend', user_data.name);
+        git_username.insertAdjacentText('beforeend', user_data.login);
+        location.insertAdjacentText('beforeend', user_data.location);
+        num_gists.insertAdjacentText('beforeend', user_data.public_gists);
 
     })
 
 }
 
+//FUNCTION: TO DISPLAY USER REPO INFORMATION
 function displayUserRepos(username) {
 
-    console.log("INSIDE DISPLAY USER REPOS");
+    console.log("INSIDE DISPLAY USER REPOS USERNAME: " + username);
     let repo_name = document.getElementById('repo-name');
     let repo_desc = document.getElementById('repo-desc');
+    let repo_conatiner = document.getElementById('repo');
     
     let request = fetch(`https://api.github.com/users/${username}/repos`);
 
-    request.then(response => console.log(response.json()))
+    request.then(response => response.json())
     .then(repos => {
 
-            repo_name.innerHTML += repos.name;
-            repo_desc.innerHTML += repos.description;
-        // for(let i = 1; i < repos.length; i++) {
+        // if there are more than 5 repos make the scroll bar visible
+        if(repos.length > 5) {
 
-        //     let next_repo_name = document.createElement("div");
-        //     let next_repo_desc = document.createElement("div");
-        //     next_repo_name.className = "repo-name-style";
-        //     next_repo_desc.className = "repo-desc-style"
-        //     repo_name.innerHTML += repos.name;
-        //     repo_desc.innerHTML += repos.description;
+            let user_repo_container = document.getElementById('repo-container');
+            user_repo_container.className = "div-scrollbar";
+        }
 
-        // }
+        for(let i = 0; i < repos.length; i++) {
 
-        // repos.array.forEach(repo => {
-            
-        //     repo_name.innerHTML += repo.name;
-        //     repo_desc.innerHTML += repo.description;
+            let next_repo_name = document.createElement("div");
+            let next_repo_desc = document.createElement("div");
 
-        // });
+            next_repo_name.innerHTML = "Name: ";
+            next_repo_desc.innerHTML = "Description: ";
+            next_repo_name.className = "repo-name-style";
+            next_repo_desc.className = "repo-desc-style";
+            next_repo_name.insertAdjacentText('beforeend', repos[i].name);
+            next_repo_desc.insertAdjacentText('beforeend', repos[i].description);
+
+            // if there is no repo description
+            if(!repos[i].description) {
+
+                next_repo_desc.insertAdjacentText('beforeend', '');
+            }
+
+            repo_conatiner.appendChild(next_repo_name);
+            repo_conatiner.appendChild(next_repo_desc);
+        }
+
+
+
     })
 
 
